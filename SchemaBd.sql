c23ab4d06ef3debe01ee45c19f995134ed06925d
Houssem-GH <hoss.gharbi@gmail.com>
2017-12-17 01:54:35 +0100

    Database creation file

---
+++ SchemaBd.sql
@@ -0,0 +1,16 @@
+-- #Creation du schema du projet web
+
+
+CREATE TABLE enzyme (EC VARCHAR(15), Officiel_Name VARCHAR(150),	
+Other_Name VARCHAR(150), Coefacteur VARCHAR(100), Commentaire VARCHAR(1500), 
+Activity VARCHAR(1000), PRIMARY KEY (EC));
+
+CREATE TABLE prosite (Id_P VARCHAR(50), EC VARCHAR(15), PRIMARY KEY (ID_P, EC),
+FOREIGN KEY  (EC) REFERENCES enzyme (EC));
+
+CREATE TABLE swissprot (Id_Sp VARCHAR(50), EC VARCHAR(15), PRIMARY KEY (ID_Sp, EC),
+FOREIGN KEY  (EC) REFERENCES enzyme (EC));
+
+CREATE TABLE publication (Titre VARCHAR(360), 	
+Year_Pub INT, EC VARCHAR(15), Auteurs VARCHAR(670) ,PRIMARY KEY (Titre, Auteurs, EC, Year_Pub), 
+FOREIGN KEY (EC) REFERENCES  enzyme (EC));
